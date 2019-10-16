8b15a942d8069f9012590615f97dc9ce99f6f62f
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2019-10-16 13:29:57 -0500

    Add fetch and show data

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,15 +1,23 @@
 
-
 const projects = () => {
-    return fetch('http://localhost:3000/info')
-        .then(response => response.json())
-        
+    fetch('http://localhost:3000/info')
+    .then(response => response.json())
+    .then(data=>showData(data))
+
+}
+const showData = (data) => {  
+    data.forEach((element, index) => {
+        $('#tableData').append(`<tr>
+        <th scope="row">${index+1}</th>
+        <td>${element.title}</td>
+        <td>${element.description}</td>    
+      </tr>`)
+    });
 
 }
 
+$(document).ready(() => {
+    projects();
+})
 
-window.onload = function () {
-    const data = projects();
-    console.log('Projects on the client side' + data);
-};
 
