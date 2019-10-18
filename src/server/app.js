f00078af8327d3cfae9dfa875da5541bde24776e
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2019-10-18 10:49:34 -0500

    Separate app and server

---
+++ src/server/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cookieSession = require('cookie-session');
+//Config files
+const config = require('./config/config');
+const port = config.port;
+const db = config.mongoose.db;
+//Model
+const User = require('./database/models/userSchema');
+//Routes
+const routes = require('./routes/routes');
+const authRoutes = require('./routes/authRoutes');
+// application
+const app = express();
+
+//Database
+mongoose.connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(`DB Connection Error: ${err.message}`);
+    });
+
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieSession(config.cookieSession));
+
+app.use(async (req, res, next) => {
+    const userId = req.session.userId;
+    if (userId) {
+        const user = await User.findById(userId);
+        if (user) {
+            res.locals.user = user;
+        } else {
+            delete req.session.userId;
+        }
+    }
+    next();
+});
+
+//static files
+app.use(express.static(config.static));
+//routes
+app.use(routes);
+app.use(authRoutes);
+app.use(morgan('dev'));
+
+// Handle errors
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  });
+
+  module.exports=app;
